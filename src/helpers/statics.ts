6042093295c439eb4c2eb9375765d51ede2aefe6
Lorenzo Bonicelli <lorenzobonicelli@libero.it>
2020-02-28 18:21:08 +0100

    Init heroku url

--- src/helpers/statics.ts
+++ src/helpers/statics.ts
@@ -1,4 +1,4 @@
-export const BASE_URI='http://192.168.0.19:3000';
+export const BASE_URI='http://kittenwars-awm.herokuapp.com';
 export const KITTENS_URI = BASE_URI + '/kittens/';
 export const VOTE_URI = BASE_URI + '/vote';
 export const LOGIN_URI = BASE_URI + '/app/login';
